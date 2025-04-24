3eabda502033b55c742c396faea4f9a36b8673bb
arnoemil <arno.toro@gmail.com>
2025-04-24 22:53:37 +0300

    changed folder names

--- folder_name.sh
+++ folder_name.sh
@@ -1,26 +1,6 @@
 #!/bin/bash
 
-# Find all directories starting with 'Week_' or 'week_'
-for d in Topic* topic*; do
-  if [ -d "$d" ]; then
-    # Lowercase version of the folder name
-    lower=$(echo "$d" | tr '[:upper:]' '[:lower:]')
-    
-    # If already lowercase, skip
-    if [ "$d" == "$lower" ]; then
-      continue
-    fi
-    
-    # Temporary rename to avoid case-only issue
-    temp="${d}_temp"
-
-    echo "Renaming $d to $temp"
-    mv "$d" "$temp"
-
-    echo "Renaming $temp to $lower"
-    mv "$temp" "$lower"
-
-    # Stage the rename
-    git add -A
-  fi
+for d in topic*_temp; do
+  temp="$topic{d}"
+  mv "$d" "$temp"
 done
